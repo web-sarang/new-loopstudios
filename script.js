76d8c9e52008d3fd815e5ccf2ad0f4aa1d45bcea
yi-rheena <77715656+yi-rheena@users.noreply.github.com>
2021-03-30 16:18:52 +0900

    add files

---
+++ script.js
@@ -0,0 +1,11 @@
+// Burger-menu
+
+const burgerMenu = document.querySelector('.menu__burger');
+const menuBody = document.querySelector('.menu__body');
+if (burgerMenu) {
+    burgerMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        burgerMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
